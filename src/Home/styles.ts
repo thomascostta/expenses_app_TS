67acddbcbddda6fba0079d450a4ce61059741102
thomascostta <thomas.jeffcosta@gmail.com>
2022-08-19 10:07:30 -0300

    update

--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -4,6 +4,10 @@ export const Container = styled.View`
   flex: 1;
   padding: 16px 16px 0;
   background-color: #E1E1EF;
-  
   padding-top: 54px;
+`;
+
+export const Chart = styled.View`
+  width: 100%;
+  align-items: center;
 `;
